12371bc84f078108db7af271eb73f71ac85a2320
hung_dev <nguyenmanhhung20184@gmail.com>
2026-04-08 23:43:25 +0700

    1

--- Assets/Scripts/Counter/TrashCounter.cs
+++ Assets/Scripts/Counter/TrashCounter.cs
@@ -7,9 +7,29 @@ public class TrashCounter : BaseCounter
     public override void Interact(Player player)
     {
         base.Interact(player);
-        if(player.HasKitchenObject())
+        if (player.HasKitchenObject())
         {
-            player.GetKitchenObject().DestroySelf();
+            KitchenObject playerObject = player.GetKitchenObject();
+
+            if (playerObject is PlateObject plate)
+            {
+                if (plate.HasKitchenObject())
+                {
+                    plate.GetKitchenObject().DestroySelf();
+                }
+            }
+            else if (playerObject is PotObject pot)
+            {
+                if (pot.HasKitchenObject())
+                {
+                    pot.GetKitchenObject().DestroySelf();
+                }
+                pot.EmptyPot();
+            }
+            else
+            {
+                playerObject.DestroySelf();
+            }
         }
     }
 }
